711e529346768bda58c3fddd22a1bc2d588a14b1
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-03-29 17:18:23 +0200

    Fix errors with rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
-
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, dependent: :delete_all, foreign_key: 'post_id'
   has_many :likes, dependent: :delete_all, foreign_key: 'post_id'
