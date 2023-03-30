1c9bc7ec0b3ea23bb57ed36c63485fd51fdf4a67
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-03-30 16:34:21 +0200

    Add validations specifications for the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :delete_all, foreign_key: 'author_id'
   has_many :comments, dependent: :delete_all, foreign_key: 'author_id'
 
+  validates :name, presence: true
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def most_recent_post
     posts.order(created_at: :desc).limit(3)
   end
