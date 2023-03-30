322ca794bfe4c38870caa0318122555ce44ccaf8
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-03-30 16:34:47 +0200

    Add validations specifications for the Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,9 @@ class Post < ApplicationRecord
 
   after_commit :update_posts_counter
 
+  validates :title, presence: true, length: { minimum: 1, maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def update_posts_counter
     author.increment!(:posts_counter)
   end
