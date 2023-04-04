1225de5bdcd0d95e0b0ed2655b5bf2161f3cef75
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-04-04 12:02:14 +0200

    Add the specifications for the UsersController

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe 'UsersController', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
+    before(:example) do
+      get '/users'
+    end
+
+    it 'return correct response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should respond with a correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'body should have correct  placeholder text' do
+      expect(response.body).to include('The list of all users')
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
-      expect(response).to have_http_status(:success)
+    before(:example) do
+      get '/users/1'
+    end
+
+    it 'return correct response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'body should includes correct placeholder text' do
+      expect(response.body).to include('Display a specific user')
     end
   end
 end
