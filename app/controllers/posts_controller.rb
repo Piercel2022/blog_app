3622104d7fa5f76e20b041dc9cca394e3facc4f9
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-04-12 09:48:21 +0200

    Add the PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   def index
-    #@posts = Post.all.freeze
     @posts = [] if @posts.nil?
     @user = User.find(params[:user_id])
     @user_posts = Post.where(author_id: params[:user_id]).order(created_at: :asc)
